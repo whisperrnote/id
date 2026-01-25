359364059fb9014a322f1c9f974e761c1bd13d6c
nathfavour <116535483+nathfavour@users.noreply.github.com>
2026-01-25 19:16:29 +0000

    ✨ feat: add alpha import from MUI in Topbar component

--- app/components/Topbar.tsx
+++ app/components/Topbar.tsx
@@ -14,6 +14,7 @@ import {
   Stack,
   Typography,
   Divider,
+  alpha,
 } from '@mui/material';
 import { Logout, Settings, ArrowBack, Fingerprint, History, Apps } from '@mui/icons-material';
 import EcosystemPortal from './EcosystemPortal';
